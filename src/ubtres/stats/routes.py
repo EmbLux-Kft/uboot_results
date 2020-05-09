512b525c8fca2b6bfab27767f6163ed20f430711
Heiko Schocher <hs@denx.de>
2020-05-09 18:16:54 +0200

    stats, routes: add SPL image
    add spl image to stats/<defconfig>/<count>
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- src/ubtres/stats/routes.py
+++ src/ubtres/stats/routes.py
@@ -1,4 +1,4 @@
-from flask import (make_response, flash, Blueprint)
+from flask import (make_response, flash, Blueprint, render_template)
 from flask_login import current_user, login_required
 from ubtres.utils import get_defconfig_data
 from ubtres import db
@@ -21,8 +21,8 @@ def side_values(num_list):
     results_list = sorted(num_list)
     return rounddown(results_list[0]), roundup(results_list[-1])
 
-@stats.route("/stats/<string:defconfig>/<int:count>")
-def stats_defconfig(defconfig, count):
+@stats.route("/stats/<string:defconfig>/<string:imgtyp>/<int:count>")
+def stats_defconfig(defconfig, imgtyp, count):
     dates, splsizes, ubsizes = get_defconfig_data(defconfig, count)
     if dates == None:
         return error_404(0)
@@ -33,17 +33,24 @@ def stats_defconfig(defconfig, count):
     ss = splsizes
     fig = Figure(figsize=(14, 9))
     axis = fig.add_subplot(1, 1, 1)
-    axis.set_title("U-Bootsize in bytes")
+    if imgtyp == 'u-boot':
+        axis.set_title("U-Boot size in bytes")
+        minv, maxv = side_values(ubsizes)
+    else:
+        axis.set_title("SPL size in bytes")
+        minv, maxv = side_values(splsizes)
     axis.set_xlabel("Commit ID")
     axis.grid(True)
     # get minimum and maximum
-    minv, maxv = side_values(ubsizes)
     axis.set_ylim([minv, maxv])
     xs = range(count)
     axis.set_xticks(xs)
     axis.set_xticklabels(dates, rotation=90)
     try:
-        axis.plot(xs, us, label=f"U-Boot")
+        if imgtyp == 'u-boot':
+            axis.plot(xs, us, label=f"U-Boot")
+        else:
+            axis.plot(xs, ss, label=f"SPL")
     except:
         return error_416(count)
 
@@ -57,3 +64,7 @@ def stats_defconfig(defconfig, count):
     response.mimetype = 'image/png'
     return response
 
+@stats.route("/stats/<string:defconfig>/<int:count>")
+def result_spl_uboot(defconfig, count):
+    data = {"defconfig" : defconfig, "count" : count }
+    return render_template('stats.html', title=f"SPL / U-Boot for {defconfig}", data=data)
